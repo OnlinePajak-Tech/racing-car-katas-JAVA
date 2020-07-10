17b18264db35cfed8af968d128c11f6eb895b4d0
Surya Setiyaputra <surya.setiyaputra@online-pajak.com>
2020-07-10 10:42:42 +1000

    update tests

--- TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
+++ TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
@@ -1,13 +1,23 @@
 package tddmicroexercises.telemetrysystem;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
-public class TelemetryDiagnosticControlsTest
-{
-    
-	@Test
-    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
-    {
+@ExtendWith(MockitoExtension.class)
+public class TelemetryDiagnosticControlsTest {
+    @Mock
+    private TelemetryClient telemetryClient;
+    @InjectMocks
+    private TelemetryDiagnosticControls telemetryDiagnosticControls;
+
+    @Test
+    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response() throws Exception {
+        //When
+        telemetryDiagnosticControls.checkTransmission();
+        //Then
+        Mockito.verify(telemetryClient).send(TelemetryClient.DIAGNOSTIC_MESSAGE);
     }
-
 }
